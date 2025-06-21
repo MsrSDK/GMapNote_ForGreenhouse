66444d15243e9bcf07319c7e28ab78292510ba84
MsrSDK <s.713193@gmail.com>
2025-06-22 05:24:26 +0900

    🩹 デフォルト値・オプション値を設定

--- app.rb
+++ app.rb
@@ -33,7 +33,7 @@ content_script site: "www.google.com/maps/*" do
         title_element.innerText = title + ":"
         grid_div.appendChild(title_element)
         case title
-        when "顧客候補名", "サービス名", "散布物", "散布対象", "情報元", "都道府県", "所在市区町村", "住所", "部署", "電話番号", "URL", "棟形式", "棟数", "ハウス面積(m2)", "備考", "記載者", "記載日"
+        when "顧客候補名", "都道府県", "所在市区町村", "住所", "部署", "電話番号", "URL", "棟数", "ハウス面積(m2)", "備考", "記載者", "記載日"
             input_element = document.createElement("input")
             input_element.type = "text"
             input_element.style.backgroundColor = "#ffffff" # 白背景
@@ -41,12 +41,79 @@ content_script site: "www.google.com/maps/*" do
             input_element.style.boxSizing = "border-box" # 幅を親要素に合わせる
             input_element.style.border = "1px solid #ccc" # 枠線
             grid_div.appendChild(input_element)
-        when "品目", "屋根形状"
+        when "サービス名"
+            input_element = document.createElement("input")
+            input_element.type = "text"
+            input_element.style.backgroundColor = "#ffffff" # 白背景
+            input_element.style.width = "100%"
+            input_element.style.boxSizing = "border-box" # 幅を親要素に合わせる
+            input_element.style.border = "1px solid #ccc" # 枠線
+            input_element.value = "PTS"
+            grid_div.appendChild(input_element)
+        when "散布物"
+            input_element = document.createElement("input")
+            input_element.type = "text"
+            input_element.style.backgroundColor = "#ffffff" # 白背景
+            input_element.style.width = "100%"
+            input_element.style.boxSizing = "border-box" # 幅を親要素に合わせる
+            input_element.style.border = "1px solid #ccc" # 枠線
+            input_element.value = "遮光剤"
+            grid_div.appendChild(input_element)
+        when "散布対象"
+            input_element = document.createElement("input")
+            input_element.type = "text"
+            input_element.style.backgroundColor = "#ffffff" # 白背景
+            input_element.style.width = "100%"
+            input_element.style.boxSizing = "border-box" # 幅を親要素に合わせる
+            input_element.style.border = "1px solid #ccc" # 枠線
+            input_element.value = "ガラス室・ハウス"
+            grid_div.appendChild(input_element)
+        when "情報元"
+            input_element = document.createElement("input")
+            input_element.type = "text"
+            input_element.style.backgroundColor = "#ffffff" # 白背景
+            input_element.style.width = "100%"
+            input_element.style.boxSizing = "border-box" # 幅を親要素に合わせる
+            input_element.style.border = "1px solid #ccc" # 枠線
+            input_element.value = "航空写真"
+            grid_div.appendChild(input_element)
+        when "品目"
+            input_element = document.createElement("input")
+            input_element.type = "text"
+            input_element.style.backgroundColor = "#ffffff" # 白背景
+            input_element.style.width = "100%"
+            input_element.style.boxSizing = "border-box" # 幅を親要素に合わせる
+            input_element.style.border = "1px solid #ccc" # 枠線
+            input_element.value = "トマト"
+            grid_div.appendChild(input_element)
+        when "棟形式"
             select_element = document.createElement("select")
             select_element.style.backgroundColor = "#ffffff"
             select_element.style.width = "100%"
             select_element.style.boxSizing = "border-box"
             grid_div.appendChild(select_element)
+            option1 = document.createElement("option")
+            option1.value = "単棟"
+            option1.textContent = "単棟"
+            select_element.appendChild(option1)
+            option2 = document.createElement("option")
+            option2.value = "連棟"
+            option2.textContent = "連棟"
+            select_element.appendChild(option2)
+        when "屋根形状"
+            select_element = document.createElement("select")
+            select_element.style.backgroundColor = "#ffffff"
+            select_element.style.width = "100%"
+            select_element.style.boxSizing = "border-box"
+            grid_div.appendChild(select_element)
+            option1 = document.createElement("option")
+            option1.value = "丸屋根型(パイプハウス等)"
+            option1.textContent = "丸屋根型(パイプハウス等)"
+            select_element.appendChild(option1)
+            option2 = document.createElement("option")
+            option2.value = "屋根型(フェンロー型等)"
+            option2.textContent = "屋根型(フェンロー型等)"
+            select_element.appendChild(option2)
         when "基本情報", "アトリビュート"
             blank_span = document.createElement("span")
             grid_div.appendChild(blank_span)
