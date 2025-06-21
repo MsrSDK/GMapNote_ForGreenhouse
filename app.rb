7e8e0ce022ca088ddeae68244716199b8ba98d4d
MsrSDK <s.713193@gmail.com>
2025-06-22 05:01:43 +0900

    ✨ 項目と記入欄を作成

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ content_script site: "www.google.com/maps/*" do
     # スタイルを適用して画面右上に固定表示
     main_div.style.position = "fixed" # 画面に固定
     main_div.style.right = "0"        # 画面右端に配置
-    main_div.style.width = "40%"
+    # main_div.style.width = "40%"
     main_div.style.color = "black"    # 文字色
     main_div.style.backgroundColor = "#e0ffff"    # 背景色
     main_div.style.margin_top = "6px"  # 上側の余白
@@ -18,4 +18,39 @@ content_script site: "www.google.com/maps/*" do
     # content-container要素配下に作成したdivを追加
     content_container = document.getElementById("content-container")
     content_container.appendChild(main_div) if content_container
+
+    grid_div = document.createElement("div")
+    grid_div.style.display = "grid"
+    grid_div.style.gap = "6px"
+    grid_div.style.gridTemplateColumns = "120px 1fr" # 2列のグリッド
+
+    title_list = ["顧客候補名", "サービス名", "散布物", "散布対象", "基本情報", "情報元", "都道府県", "所在市区町村", "住所", "部署", "電話番号", "URL", "アトリビュート", "品目", "棟形式", "屋根形状", "棟数", "ハウス面積(m2)", "備考", "記載者", "記載日"]
+    title_list.each do |title|
+        # タイトルを作成
+        title_element = document.createElement("span")
+        title_element.textContent = title
+        title_element.style.textAlign = "right"
+        title_element.innerText = title + ":"
+        grid_div.appendChild(title_element)
+        case title
+        when "顧客候補名", "サービス名", "散布物", "散布対象", "情報元", "都道府県", "所在市区町村", "住所", "部署", "電話番号", "URL", "棟形式", "棟数", "ハウス面積(m2)", "備考", "記載者", "記載日"
+            input_element = document.createElement("input")
+            input_element.type = "text"
+            input_element.style.backgroundColor = "#ffffff" # 白背景
+            input_element.style.width = "100%"
+            input_element.style.boxSizing = "border-box" # 幅を親要素に合わせる
+            input_element.style.border = "1px solid #ccc" # 枠線
+            grid_div.appendChild(input_element)
+        when "品目", "屋根形状"
+            select_element = document.createElement("select")
+            select_element.style.backgroundColor = "#ffffff"
+            select_element.style.width = "100%"
+            select_element.style.boxSizing = "border-box"
+            grid_div.appendChild(select_element)
+        when "基本情報", "アトリビュート"
+            blank_span = document.createElement("span")
+            grid_div.appendChild(blank_span)
+        end
+        main_div.appendChild(grid_div)
+    end
 end
