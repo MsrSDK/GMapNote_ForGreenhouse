02c3773f099528c0ec96d0a0edd477fdda34e43d
MsrSDK <s.713193@gmail.com>
2025-06-22 02:15:03 +0900

    🎉 init unloosen

---
+++ app.rb
@@ -0,0 +1,8 @@
+# app.rb
+require "unloosen"
+
+content_script site: "www.example.com" do
+    # h1 = document.getElementById("")
+    h1 = document.querySelector("h1")
+    h1.innerText = "Unloosen Example Page!"
+end
