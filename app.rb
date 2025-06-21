f352d0a2521ccc13d45474d87191fd47da49b5a6
MsrSDK <s.713193@gmail.com>
2025-06-22 03:58:37 +0900

    🩹 パネルの位置を調整

--- app.rb
+++ app.rb
@@ -3,20 +3,19 @@ require "unloosen"
 
 content_script site: "www.google.com/maps/*" do
     main_div = document.createElement("div")
-    main_div.id = "unloosen-bottom-div"
-    main_div.innerText = "Hello from Unloosen! This is at the bottom of the screen."
+    main_div.id = "unloosen-main-div"
 
-    # スタイルを適用して画面下部に固定表示
+    # スタイルを適用して画面右上に固定表示
     main_div.style.position = "fixed" # 画面に固定
-    main_div.style.bottom = "0"       # 画面下部に配置
-    main_div.style.left = "0"         # 画面左端に配置
-    main_div.style.width = "100%"     # 画面幅いっぱいに広げる
+    main_div.style.right = "0"        # 画面右端に配置
+    main_div.style.width = "40%"
     main_div.style.color = "black"    # 文字色
-    main_div.style.backgroundColor = "white"    # 背景色
-    main_div.style.textAlign = "center" # テキストを中央揃え
-    main_div.style.padding = "8px"    # 内側の余白
+    main_div.style.backgroundColor = "#e0ffff"    # 背景色
+    main_div.style.margin_top = "6px"  # 上側の余白
+    main_div.style.padding = "6px"    # 内側の余白
     main_div.style.zIndex = "999"    # 他の要素より手前に表示
 
-    # 4. body要素に作成したdivを追加
-    document.body.appendChild(main_div)
+    # content-container要素配下に作成したdivを追加
+    content_container = document.getElementById("content-container")
+    content_container.appendChild(main_div) if content_container
 end
